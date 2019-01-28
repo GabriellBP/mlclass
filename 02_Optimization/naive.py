32879d2101f0eec6db7a7c4c92ca6ac8c5ef95b5
BuildTools <unconfigured@null.spigotmc.org>
2019-01-28 18:41:36 -0300

    atividade 2: método naive implementado (melhor resultado! 28.363570477928704)

--- 02_Optimization/naive.py
+++ 02_Optimization/naive.py
@@ -1,29 +1,49 @@
+import datetime
+
 import requests
 
 url = 'http://localhost:8080/antenna/simulate?phi1={}&theta1={}&phi2={}&theta2={}&phi3={}&theta3={}'
-best_result = -1000
-best_angles = {}
+angles = ['phi1', 'theta1', 'phi2', 'theta2', 'phi3', 'theta3']
 
-for phi1 in range(0, 360):
-    for theta1 in range(0, 360):
-        for phi2 in range(0, 360):
-            for theta2 in range(0, 360):
-                for phi3 in range(0, 360):
-                    for theta3 in range(0, 360):
-                        r = requests.get(url.format(phi1, theta1, phi2, theta2, phi3, theta3))
-                        result = float(r.text.split('\n')[0])
-                        
-                        if result > best_result:
-                            best_result = result
-                            
-                            best_angles['phi1'] = phi1
-                            best_angles['theta1'] = theta1
-                            best_angles['phi2'] = phi2
-                            best_angles['theta2'] = theta2
-                            best_angles['phi3'] = phi3
-                            best_angles['theta3'] = theta3
-                            
-                            print('-'*30)
-                            print('NEW Best Result:', best_result)
-                            print('NEW Best Angles:', best_angles)
-                            print()
+values = {'phi1': 10, 'theta1': 180, 'phi2': 121, 'theta2': 0, 'phi3': 180, 'theta3': 155}
+best_result = 28.363570477928704
+
+while True:
+    improved = False
+
+    for k in angles:
+        print('Current Angle:', k)
+        print('At:', datetime.datetime.now().time())
+        print()
+        best_angle = values[k]
+
+        for i in range(360):
+            values[k] = i
+
+            while True:
+                try:
+                    r = requests.get(url.format(values['phi1'], values['theta1'], values['phi2'], values['theta2'],
+                                                values['phi3'], values['theta3']))
+                    break
+                except:
+                    print('-'*15, 'DEU PAU NA REQUISIÇÃO!')
+                    continue
+
+            result = float(r.text.split('\n')[0])
+
+            if result > best_result:
+                best_result = result
+                best_angle = i
+                improved = True
+
+                print('-'*30)
+                print('NEW Best Result:', best_result)
+                print('Values:', values)
+                print('At:', datetime.datetime.now().time())
+                print()
+
+        values[k] = best_angle
+
+    if not improved:
+        print('FIM!')
+        break
