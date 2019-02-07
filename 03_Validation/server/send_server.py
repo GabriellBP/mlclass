593ef40aba2c16dbc09180c09a894e2f2e1a4763
GabriellBP <gbp@ic.ufal.br>
2019-02-07 10:12:40 -0300

    Method to send to server

---
+++ 03_Validation/server/send_server.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+
+def send_2_server(y_pred):
+    print("olá olá olá")
+    # Enviando previsões realizadas com o modelo para o servidor
+    URL = "https://aydanomachado.com/mlclass/03_Validation.php"
+
+    # TODO Substituir pela sua chave aqui
+    DEV_KEY = "Dual Core"
+
+    # json para ser enviado para o servidor
+    data = {'dev_key': DEV_KEY,
+            'predictions': pd.Series(y_pred).to_json(orient='values')}
+
+    # Enviando requisição e salvando o objeto resposta
+    r = requests.post(url=URL, data=data)
+
+    # Extraindo e imprimindo o texto da resposta
+    pastebin_url = r.text
+    print(" - Resposta do servidor:\n", r.text, "\n")
